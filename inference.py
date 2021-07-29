3a701cb21aa5265e9948977347a06a6c96d9c41e
ligaripash <ligaripash@yahoo.com>
2021-07-29 10:39:27 +0000

    98 points format

--- inference.py
+++ inference.py
@@ -5,7 +5,7 @@ from __future__ import division
 from __future__ import print_function
 
 import common_params
-from eval_params import *
+from inference_params import *
 from datetime import datetime
 
 import data_provider
